95d61fa5d673ace589d5101f823ff76ff5d790f8
Gergely Németh <ngerle@gmail.com>
2017-05-15 23:53:14 +0200

    Structure the database connection into a function

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 import psycopg2
 
 
+def connect_database():
+    try:
+        # setup connection string
+        connect_str = "dbname='gergo' user='gergo' host='localhost' password='Jn3.16'"
+        # use our connection values to establish a connection
+        conn = psycopg2.connect(connect_str)
+        # set autocommit option, to do every query when we call it
+        conn.autocommit = True
+        # create a psycopg2 cursor that can execute queries
+        cursor = conn.cursor()
+    except Exception as e:
+        print("Uh oh, can't connect. Invalid dbname, user or password?")
+        print(e)
+
+    return cursor
+
+
 def print_table(rows):
     for row in rows:
         row = str(row).strip("()").split(", ")
@@ -12,23 +29,10 @@ def print_table(rows):
 
 
 def main():
-    try:
-        # setup connection string
-        connect_str = "dbname='gergo' user='gergo' host='localhost' password='Jn3.16'"
-        # use our connection values to establish a connection
-        conn = psycopg2.connect(connect_str)
-        # set autocommit option, to do every query when we call it
-        conn.autocommit = True
-        # create a psycopg2 cursor that can execute queries
-        cursor = conn.cursor()
-        # removing the test table if it already exists
-        cursor.execute("""SELECT * FROM mentors;""")
-        # Fetch and print the result of the last execution
-        rows = cursor.fetchall()
-        print_table(rows)
-    except Exception as e:
-        print("Uh oh, can't connect. Invalid dbname, user or password?")
-        print(e)
+    cursor = connect_database()
+    cursor.execute("""SELECT * FROM mentors;""")
+    rows = cursor.fetchall()
+    print_table(rows)
 
 if __name__ == '__main__':
     main()
